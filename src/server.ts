84b5466ce0f834ea447cbdc17d8336b6149f61ca
calebcr0ss <ilay180120111@gmail.com>
2026-09-04 21:32:52 +0300

    added res end

--- src/server.ts
+++ src/server.ts
@@ -3,47 +3,59 @@ import Busboy from "busboy";
 import fs from "fs";
 
 const server = createServer((req, res) => {
-  if (req.method === "GET" && req.url === "/api/health") {
-    res.writeHead(200, {
-      "Content-Type": "application/json",
-    });
+        if (req.method === "GET" && req.url === "/api/health") {
+                res.writeHead(200, {
+                        "Content-Type": "application/json",
+                });
 
-    res.end(
-      JSON.stringify({
-        message: "Seems fine",
-      }),
-    );
+                res.end(
+                        JSON.stringify({
+                                message: "Seems fine",
+                        }),
+                );
 
-    return;
-  } else if (req.method === "POST" && req.url === "/api/photos") {
-    const busboy = Busboy({ headers: req.headers });
-    req.pipe(busboy);
+                return;
+        } else if (req.method === "POST" && req.url === "/api/photos") {
+                const busboy = Busboy({ headers: req.headers });
+                req.pipe(busboy);
 
-    busboy.on("field", (name, value) => {
-      console.log(name, value); // get a string field
-    });
+                busboy.on("field", (name, value) => {
+                        console.log(name, value); // get a string field
+                });
 
-    busboy.on("file", (name, file, info) => {
-      console.log(name); // "image"
-      console.log(info.filename); // "photo.jpg"
-      console.log(info.mimeType); // "image/jpeg"
+                busboy.on("file", (name, file, info) => {
+                        console.log(name); // "image"
+                        console.log(info.filename); // "photo.jpg"
+                        console.log(info.mimeType); // "image/jpeg"
 
-      file.pipe(fs.createWriteStream(`/data/etsik/photos/${info.filename}`));
-    });
-    return;
-  }
+                        file.pipe(fs.createWriteStream(`/data/etsik/photos/${info.filename}`));
+                });
 
-  res.writeHead(404, {
-    "Content-Type": "application/json",
-  });
+                busboy.on("finish", () => {
+                        res.writeHead(404, {
+                                "Content-Type": "application/json",
+                        });
 
-  res.end(
-    JSON.stringify({
-      error: "Not Found",
-    }),
-  );
+                        res.end(
+                                JSON.stringify({
+                                        error: "Not Found",
+                                }),
+                        );
+                })
+                return;
+        }
+
+        res.writeHead(404, {
+                "Content-Type": "application/json",
+        });
+
+        res.end(
+                JSON.stringify({
+                        error: "Not Found",
+                }),
+        );
 });
 
 server.listen(3000, "0.0.0.0", () => {
-  console.log("Server running on http://localhost:3000");
+        console.log("Server running on http://localhost:3000");
 });
