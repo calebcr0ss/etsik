45adafc81e349fdc87076799aee127923364291e
calebcr0ss <ilay180120111@gmail.com>
2026-09-04 20:41:00 +0300

    Added busboy & test photos accept

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,47 @@
 import { createServer } from "node:http";
+import Busboy from "busboy";
+import fs from "fs";
 
 const server = createServer((req, res) => {
-	if (req.method === "GET" && req.url === "/api/health") {
-		res.writeHead(200, {
-			"Content-Type": "application/json"
-		});
+  if (req.method === "GET" && req.url === "/api/health") {
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+    });
 
-		res.end(JSON.stringify({
-			message: "Seems fine"
-		}));
+    res.end(
+      JSON.stringify({
+        message: "Seems fine",
+      }),
+    );
 
-		return;
-	} else if (req.method === "GET" && req.url === "/api/photos") {
-		
-	}
+    return;
+  } else if (req.method === "GET" && req.url === "/api/photos") {
+    const busboy = Busboy({ headers: req.headers });
 
-	res.writeHead(404, {
-		"Content-Type": "application/json"
-	});
+    busboy.on("field", (name, value) => {
+      console.log(name, value); // get a string field
+    });
 
-	res.end(JSON.stringify({
-		error: "Not Found"
-	}));
+    busboy.on("file", (name, file, info) => {
+      console.log(name); // "image"
+      console.log(info.filename); // "photo.jpg"
+      console.log(info.mimeType); // "image/jpeg"
+
+      file.pipe(fs.createWriteStream("/data/etsik/photos/"));
+    });
+  }
+
+  res.writeHead(404, {
+    "Content-Type": "application/json",
+  });
+
+  res.end(
+    JSON.stringify({
+      error: "Not Found",
+    }),
+  );
 });
 
-server.listen(3000, '0.0.0.0', () => {
-	console.log("Server running on http://localhost:3000");
+server.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on http://localhost:3000");
 });
-
-
