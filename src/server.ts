3596073478bc704caa26fe6e78201a5d5598f993
calebcr0ss <ilay180120111@gmail.com>
2026-09-04 16:40:39 +0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import { createServer } from "node:http";
+
+const server = createServer((req, res) => {
+	if (req.method === "GET" && req.url === "/api/health") {
+		res.writeHead(200, {
+			"Content-Type": "application/json"
+		});
+
+		res.end(JSON.stringify({
+			message: "Seems fine"
+		}));
+
+		return;
+	} else if (req.method === "GET" && req.url === "/api/photos") {
+		
+	}
+
+	res.writeHead(404, {
+		"Content-Type": "application/json"
+	});
+
+	res.end(JSON.stringify({
+		error: "Not Found"
+	}));
+});
+
+server.listen(3000, () => {
+	console.log("Server running on http://localhost:3000");
+});
